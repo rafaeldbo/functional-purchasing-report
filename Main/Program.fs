42093ac58143f7c8eaee3cebf769c2dce739ee04
rafaeldbo <rafaeldbo@al.insper.edu.br>
2026-03-26 04:36:20 +0000

    Main lendo e salvando csv

--- Main/Program.fs
+++ Main/Program.fs
@@ -1,2 +1,25 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+open ETL.CSVHandler
+open ETL.Models
+open ETL.Parsing
+open ETL.Report
+
+
+let orders = readCSV (buildPath "../Data/order.csv")
+            |> Seq.map (unpackOrderCSVRow >> parseOrder >> tryMakeOrder)
+            |> Seq.choose id
+            |> Seq.toList
+
+let orderItems = readCSV (buildPath "../Data/order_item.csv")
+                |> Seq.map (unpackOrderItemCSVRow >> parseOrderItem >> tryMakeOrderItem)
+                |> Seq.choose id
+                |> Seq.toList
+
+let report = reportOrderTotals Pending Online orders orderItems
+
+writeCSV 
+    ["order_id"; "total_amount"; "total_taxes"] 
+    (buildPath "report.csv") 
+    (fun (r: PurchasingReport) -> [r.OrderId.ToString(); r.TotalAmount.ToString(); r.TotalTaxes.ToString()]) 
+    report
